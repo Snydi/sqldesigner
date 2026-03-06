c5b25ee672514bc775d1c5f3c06fc3f1f25d5ab2
snydi <d-snyatkov@mail.ru>
2026-03-06 14:20:19 +0300

    refactor: shortened auth code and added auto-login when registering

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -4,67 +4,65 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
-use App\Mail\Admin\NewUserRegistrationMail;
-use App\Models\User;
+use App\Services\AuthService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Mail;
-use Throwable;
 
 class AuthController extends Controller
 {
+
+    protected AuthService $authService;
+
+    public function __construct(AuthService $authService)
+    {
+        $this->authService = $authService;
+    }
+
     public function register(RegisterRequest $request): JsonResponse
     {
-        try {
-            $user = new User([
-                'email' => $request->email,
-                'password' => Hash::make($request->password),
-            ]);
-            $user->save();
-            Mail::to(config('mail.from.address'))->send(new NewUserRegistrationMail($request->email)); //TODO add this to queue, so the is no load time on register
+        $status = $this->authService->register($request->validated());
+        if (!Auth::attempt($request->only(['email', 'password']))) {
             return response()->json([
-                'status' => true,
-                'message' => 'Registered successfully'
-            ]);
-
-        } catch (Throwable $th) {
-            return response()->json([
-                'message' => $th->getMessage()
-            ], 500);
+                'status' => false,
+                'message' => 'Wrong email or password',
+            ], 401);
         }
+        $token = $this->authService->login($request->validated());
+        return response()->json([
+            'status' => $status,
+            'token' => $token,
+            'message' => 'Registered successfully',
+        ]);
     }
 
     public function login(LoginRequest $request): JsonResponse
     {
-        try {
-            if (!Auth::attempt($request->only(['email', 'password']))) {
-                return response()->json([
-                    'status' => false,
-                    'message' => 'Wrong email or password',
-                ], 401);
-            }
-
-            $user = User::where('email', $request->email)->first();
-            $token = preg_replace('/(\d\|)(.+)/', '$2', $user->createToken("API TOKEN")->plainTextToken);
-
+        if (!Auth::attempt($request->only(['email', 'password']))) {
             return response()->json([
-                'message' => 'Logged in successfully',
-                'token' => $token
-            ]);
-        } catch (Throwable $th) {
-            return response()->json([
-                'message' => $th->getMessage()
-            ], 500);
+                'status' => false,
+                'message' => 'Wrong email or password',
+            ], 401);
         }
+
+        return response()->json([
+            'status' => true,
+            'token' => $this->authService->login($request->validated()),
+            'message' => 'Logged in successfully',
+        ]);
     }
 
     public function logout(Request $request): JsonResponse
     {
-        $request->user()->tokens()->delete();
-        Auth::guard('web')->logout();
-        return response()->json(['message' => 'Logged out successfully']);
+        return response()->json([
+            'status' => $this->authService->logout($request->user()),
+            'message' => 'Logged out successfully'
+        ]);
+    }
+
+    private function checkCredentials(array $credentials)
+    {
+        //TODO доделать
     }
 }
---
+++ backend/app/Services/AuthService.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Services;
+
+use App\Mail\Admin\NewUserRegistrationMail;
+use App\Models\User;
+use App\Repositories\AuthRepository;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
+
+class AuthService
+{
+    protected AuthRepository $authRepository;
+
+    public function __construct(AuthRepository $authRepository)
+    {
+        $this->authRepository = $authRepository;
+    }
+    public function register(array $data): true
+    {
+        $this->authRepository->createNewUser($data);
+        Mail::to(config('mail.from.address'))->send(new NewUserRegistrationMail($data['email'])); //TODO add this to queue, so the is no load time on register
+        return true;
+    }
+
+    public function login(array $data): string
+    {
+        $user = $this->authRepository->findUser($data['email']);
+        return preg_replace('/(\d\|)(.+)/', '$2', $user->createToken("API TOKEN")->plainTextToken);
+    }
+
+    public function logout(User $user): true
+    {
+        $user->tokens()->delete();
+        Auth::guard('web')->logout();
+        return true;
+    }
+}
