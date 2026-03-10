b1f844d8e06fdf36f5dc0170102352565e384cb1
snydi <d-snyatkov@mail.ru>
2026-03-10 18:54:50 +0300

    feat: changed favicon and title

--- backend/resources/views/home.blade.php
+++ backend/resources/views/home.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Snydiagram</title>
+    <title>SQL-designer</title>
     @vite(['src/css/app.css'])
 </head>
 <body>
