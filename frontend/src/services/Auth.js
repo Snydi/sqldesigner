2d25438952892c7483d1a92a0b2e53abd86eee40
snydi <d-snyatkov@mail.ru>
2026-03-10 11:56:18 +0300

    fix: fixed auth errors not appearing

--- frontend/src/services/Auth.js
+++ frontend/src/services/Auth.js
@@ -3,14 +3,16 @@ import router from '../router/index.js'
 import store from '@/store/index.js'
 import axios from '@/axios'
 
-const $toast = useToast()
-
 async function authenticate(endpoint, userData) {
-    await axios.get('/sanctum/csrf-cookie')
-    const response = await axios.post(endpoint, { email: userData.email, password: userData.password })
-    store.commit('login', response.data.token)
-    $toast.success(response.data.message)
-    await router.push({ name: 'diagrams' })
+    const $toast = useToast({ position: 'bottom-right' })
+    try {
+        const response = await axios.post(endpoint, { email: userData.email, password: userData.password })
+        $toast.success(response.data.message)
+        store.commit('login', response.data.token)
+        await router.push({ name: 'diagrams' })
+    } catch (error) {
+        $toast.error(error.response?.data?.message ?? 'An error occurred')
+    }
 }
 
 export const Auth = {
@@ -18,6 +20,7 @@ export const Auth = {
     login: (userData) => authenticate('/api/login', userData),
 
     async logout() {
+        const $toast = useToast({ position: 'bottom-right' })
         const response = await axios.post('/api/logout')
         store.commit('logout')
         response.status ? $toast.success(response.data.message) : $toast.error(response.data.message)
