5a7efe00681bda89a6a26a0fbecf949766a6df10
snydi <d-snyatkov@mail.ru>
2026-03-10 16:18:03 +0300

    fix: urgent prod build fixes

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-chown -R www:www /var/www/html/backend/storage /var/www/html/backend/bootstrap/cache
-exec su-exec www php-fpm
+  chown -R www:www /var/www/html/backend/storage /var/www/html/backend/bootstrap/cache
+  exec php-fpm
