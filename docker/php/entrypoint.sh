e86b488b8207750b7c21326ff404a9ef0880f580
snydi <d-snyatkov@mail.ru>
2026-03-11 13:04:51 +0300

    fix: fixed permission error

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-  chown -R www:www /var/www/html/backend/storage /var/www/html/backend/bootstrap/cache
+  chmod -R 777 /var/www/html/backend/storage /var/www/html/backend/bootstrap/cache
   exec php-fpm
