a0a2d50849b5589e8c0df50bda40b77190660eed
snydi <d-snyatkov@mail.ru>
2026-03-13 13:29:41 +0300

    feat: improved sql modal css

--- frontend/src/components/SqlModal.vue
+++ frontend/src/components/SqlModal.vue
@@ -1,9 +1,16 @@
 <template>
     <div class="modal flex-centered">
         <div class="sql_modal_content">
-            <textarea class="sql_textarea" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)"></textarea>
-            <button class="btn btn-primary" @click="$emit('primary-action')">{{ primaryLabel }}</button>
-            <button class="btn btn-primary" @click="$emit('close')">Close</button>
+            <div class="sql_modal_header">
+                <span class="sql_modal_title">{{ primaryLabel }} SQL</span>
+                <button class="sql_modal_close" @click="$emit('close')">
+                    <img src="../icons/close.svg" alt="Close">
+                </button>
+            </div>
+            <textarea class="sql_textarea" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" :placeholder="primaryLabel === 'Import' ? 'Paste your SQL here...' : ''"></textarea>
+            <div class="sql_modal_footer">
+                <button class="btn btn-primary" @click="$emit('primary-action')">{{ primaryLabel }}</button>
+            </div>
         </div>
     </div>
 </template>
